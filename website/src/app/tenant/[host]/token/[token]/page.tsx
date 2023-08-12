53cd912ec78c793fe360fd69575edcf8be33c2f2
remorses <beats.by.morse@gmail.com>
2023-08-12 12:23:20 +0200

    website/src/app/tenant/[host]/token/[token]/page: fix

--- website/src/app/tenant/[host]/token/[token]/page.tsx
+++ website/src/app/tenant/[host]/token/[token]/page.tsx
@@ -3,6 +3,6 @@ import { ChooseProvider } from '@/app/tenant/[host]/token/[token]/ChooseProvider
 import { ProviderSetupParams, providerSetupContext } from '@/lib/hooks'
 import { jwtVerify } from 'jose'
 
-export default async function Page({ params: { token, host } }) {
+export default function Page({ params: { token, host } }) {
     return <ChooseProvider />
 }
