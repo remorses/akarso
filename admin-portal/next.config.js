c3601463c1e1a17d73d55612ee38c38ad05b8eac
remorses <beats.by.morse@gmail.com>
2023-08-12 13:59:28 +0200

    admin-portal/next.config:

--- admin-portal/next.config.js
+++ admin-portal/next.config.js
@@ -1,10 +1,16 @@
 const { withServerActions } = require('server-actions-for-next-pages')
 
+const piped = pipe(withServerActions())
+
 /** @type {import('next').NextConfig} */
-const nextConfig = withServerActions()({
+const nextConfig = piped({
     experimental: {
         externalDir: true,
     },
 })
+
+function pipe(...fns) {
+    return (x) => fns.reduce((v, f) => f(v), x)
+}
 
 module.exports = nextConfig
