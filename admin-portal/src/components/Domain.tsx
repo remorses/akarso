00d628e481f6eb5bb6be21a1632db210c8524013
remorses <beats.by.morse@gmail.com>
2023-08-13 14:45:59 +0200

    admin-portal/src/components/Domain: validate domain

--- admin-portal/src/components/Domain.tsx
+++ admin-portal/src/components/Domain.tsx
@@ -5,11 +5,13 @@ import { Button, Input } from '@nextui-org/react'
 import { Container } from 'admin-portal/src/components/Container'
 import Link from 'next/link'
 import { useParams, useRouter } from 'next/navigation'
+import { useState } from 'react'
 
 export function Domain() {
     const domain = useStore(domainAtom)
     const { host, token } = useParams()!
     const router = useRouter()
+    const [error, setError] = useState<string>('')
 
     return (
         <Container>
@@ -22,13 +24,25 @@ export function Domain() {
             >
                 <div className=''>What is your SSO domain?</div>
                 <Input
-                    onValueChange={(x) => domainAtom.set(x)} // TODO validate domain
+                    onValueChange={(x) => {
+                        if (!x.includes('.')) {
+                            setError('Invalid domain')
+                        } else {
+                            setError('')
+                        }
+                        domainAtom.set(x)
+                    }}
                     value={domain}
                     type='text'
                     placeholder='example.com'
                 />
+                {error && <div className='text-red-400 text-sm'>{error}</div>}
                 <Link legacyBehavior href={`/token/${token}/select-provider`}>
-                    <Button isDisabled={!domain} color='primary' type='submit'>
+                    <Button
+                        isDisabled={!domain || !!error}
+                        color='primary'
+                        type='submit'
+                    >
                         Continue
                     </Button>
                 </Link>
