34d7e1f24e75aa429ae370ff2d68bf3ed8f631eb
remorses <beats.by.morse@gmail.com>
2023-08-13 22:34:18 +0200

    website/src/pages/demo-portal: nn

--- website/src/pages/demo-portal.tsx
+++ website/src/pages/demo-portal.tsx
@@ -10,13 +10,15 @@ export default function Page() {
 
 export const getServerSideProps = (async (ctx: GetServerSidePropsContext) => {
     const secret = DEMO_SITE_SECRET
+    const slug = 'demo'
     const site = await prisma.site.upsert({
         where: {
-            secret,
+            // secret,
+            slug,
         },
         create: {
             secret,
-            slug: 'demo',
+            slug,
             orgId: env.DEMO_ORG_ID!,
             logoUrl:
                 'https://edlzdykcnsqevfjzscic.supabase.co/storage/v1/object/public/user-uploads/e8a59d6c-2e43-4083-b3f1-59b83612292c/3ff3c770-81f5-4c0b-ac68-662d75e0e263-logo_with_text_black%402x-png',
@@ -28,7 +30,10 @@ export const getServerSideProps = (async (ctx: GetServerSidePropsContext) => {
             startUrl: 'https://xxx',
             websiteUrl: 'https://akarso.co',
         },
-        update: {},
+        update: {
+            slug,
+            secret,
+        },
     })
     const { url } = await createSessionUrl({
         callbackUrl: env.NEXT_PUBLIC_URL,
