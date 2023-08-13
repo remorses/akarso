889845691bd4b15b3f97403de71e4dbbf09168c2
remorses <beats.by.morse@gmail.com>
2023-08-13 22:44:16 +0200

    website/src/pages/login:

--- website/src/pages/login.tsx
+++ website/src/pages/login.tsx
@@ -1,6 +1,7 @@
 import { useSupabaseClient, useUser } from '@supabase/auth-helpers-react'
 import router from 'next/router'
 import { useEffect } from 'react'
+import { MyNavbar } from 'website/src/components/specific'
 import { createLoginRedirectUrl } from 'website/src/lib/utils'
 
 const Login = () => {
@@ -29,7 +30,11 @@ const Login = () => {
         }
     }, [user])
 
-    return null
+    return (
+        <div className='w-full flex flex-col max-w-[1200px] mx-auto'>
+            <MyNavbar />
+        </div>
+    )
 }
 
 export default Login
