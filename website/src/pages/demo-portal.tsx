9655e23bc6e060b12cf65d47ec193fc423f6631f
remorses <beats.by.morse@gmail.com>
2023-08-13 22:36:11 +0200

    website/src/pages/demo-portal: fix portal demo

--- website/src/pages/demo-portal.tsx
+++ website/src/pages/demo-portal.tsx
@@ -11,6 +11,17 @@ export default function Page() {
 export const getServerSideProps = (async (ctx: GetServerSidePropsContext) => {
     const secret = DEMO_SITE_SECRET
     const slug = 'demo'
+    const data = {
+        logoUrl:
+            'https://edlzdykcnsqevfjzscic.supabase.co/storage/v1/object/public/user-uploads/e8a59d6c-2e43-4083-b3f1-59b83612292c/cc8ca374-ef8f-4b73-ba06-9488277f6924-logo-svg',
+        supabaseAccessToken: 'https://example.com',
+        supabaseProjectRef: 'example.com',
+        acsUrl: 'https://example.com',
+        entityId: 'https://example.com',
+        relayState: 'https://example.com',
+        startUrl: 'https://example.com',
+        websiteUrl: 'https://akarso.co',
+    }
     const site = await prisma.site.upsert({
         where: {
             // secret,
@@ -20,19 +31,12 @@ export const getServerSideProps = (async (ctx: GetServerSidePropsContext) => {
             secret,
             slug,
             orgId: env.DEMO_ORG_ID!,
-            logoUrl:
-                'https://edlzdykcnsqevfjzscic.supabase.co/storage/v1/object/public/user-uploads/e8a59d6c-2e43-4083-b3f1-59b83612292c/cc8ca374-ef8f-4b73-ba06-9488277f6924-logo-svg',
-            supabaseAccessToken: 'https://example.com',
-            supabaseProjectRef: 'example.com',
-            acsUrl: 'https://example.com',
-            entityId: 'https://example.com',
-            relayState: 'https://example.com',
-            startUrl: 'https://example.com',
-            websiteUrl: 'https://akarso.co',
+            ...data,
         },
         update: {
             slug,
             secret,
+            ...data,
         },
     })
     const { url } = await createSessionUrl({
