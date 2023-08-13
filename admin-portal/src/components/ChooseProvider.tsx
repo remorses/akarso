47a3dc8a7537f6403332c7df54230fe0ef40094c
remorses <beats.by.morse@gmail.com>
2023-08-13 15:20:05 +0200

    admin-portal/src/components/ChooseProvider: add tooltip

--- admin-portal/src/components/ChooseProvider.tsx
+++ admin-portal/src/components/ChooseProvider.tsx
@@ -3,7 +3,14 @@ import { Container } from 'admin-portal/src/components/Container'
 import { RadioCard } from 'admin-portal/src/components/form'
 import { providers } from 'admin-portal/src/lib/providers'
 import { createStepPath } from 'admin-portal/src/lib/utils'
-import { Button, Checkbox, Radio, RadioGroup, cn } from '@nextui-org/react'
+import {
+    Button,
+    Checkbox,
+    Radio,
+    RadioGroup,
+    Tooltip,
+    cn,
+} from '@nextui-org/react'
 import { useParams, useRouter } from 'next/navigation'
 import { useState } from 'react'
 
@@ -30,13 +37,23 @@ export function ChooseProvider() {
                 >
                     {Object.entries(providers).map(([key, value]) => {
                         const disabled = 'inactive' in value && value.inactive
+                        if (disabled) {
+                            return (
+                                <Tooltip showArrow key={key} content='Coming Soon'>
+                                    <div className=''>
+                                        <RadioCard
+                                            isDisabled={disabled}
+                                            icon={value.icon}
+                                            value={key}
+                                        >
+                                            {value.name}
+                                        </RadioCard>
+                                    </div>
+                                </Tooltip>
+                            )
+                        }
                         return (
-                            <RadioCard
-                                isDisabled={disabled}
-                                key={key}
-                                icon={value.icon}
-                                value={key}
-                            >
+                            <RadioCard key={key} icon={value.icon} value={key}>
                                 {value.name}
                             </RadioCard>
                         )
