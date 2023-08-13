2f4b4ca038e0b4a3d55083e68112300c7850754a
remorses <beats.by.morse@gmail.com>
2023-08-13 15:03:23 +0200

    admin-portal/src/app/tenant/[host]/token/[token]/provider/[provider]/step/[step]/page: added back link

--- admin-portal/src/app/tenant/[host]/token/[token]/provider/[provider]/step/[step]/page.tsx
+++ admin-portal/src/app/tenant/[host]/token/[token]/provider/[provider]/step/[step]/page.tsx
@@ -2,6 +2,7 @@
 import { useStore } from '@nanostores/react'
 
 import {
+    ArrowLeftIcon,
     ArrowRight,
     ArrowRightIcon,
     CheckCircle2,
@@ -63,97 +64,112 @@ export default function Page({
     // console.log({ disabled, metadataUrl, metadataXml })
     return (
         <Container>
-            <div className='flex h-full justify-between gap-12'>
-                <div className='flex flex-col gap-6 shrink-0 leading-relaxed max-w-[800px]'>
-                    <h1 className='font-semibold text-2xl'>{stepObj.title}</h1>
-                    {stepObj.content}
-                    <div className='flex'>
-                        {/* <div className='grow'></div> */}
-                        {!isEnd ? (
-                            <Link
-                                legacyBehavior
-                                href={createStepPath({
-                                    host,
-                                    provider,
-                                    step: step + 1,
-                                    hash,
-                                })}
-                            >
+            <div className='space-y-2'>
+                <div className=''>
+                    <Link
+                        className='flex items-center hover:underline text-sm gap-2'
+                        href={`/token/${token}/select-provider`}
+                    >
+                        <ArrowLeftIcon className='w-3' />
+                        Back to providers
+                    </Link>
+                </div>
+                <div className='flex h-full justify-between gap-12'>
+                    <div className='flex flex-col gap-6 shrink-0 leading-relaxed max-w-[800px]'>
+                        <h1 className='font-semibold text-2xl'>
+                            {stepObj.title}
+                        </h1>
+                        {stepObj.content}
+                        <div className='flex'>
+                            {/* <div className='grow'></div> */}
+                            {!isEnd ? (
+                                <Link
+                                    legacyBehavior
+                                    href={createStepPath({
+                                        host,
+                                        provider,
+                                        step: step + 1,
+                                        hash,
+                                    })}
+                                >
+                                    <Button
+                                        type='button'
+                                        endContent={
+                                            <ArrowRightIcon className='w-4' />
+                                        }
+                                        isDisabled={disabled}
+                                        color='primary'
+                                        // color='success'
+                                        // variant='flat'
+                                    >
+                                        Next Step
+                                    </Button>
+                                </Link>
+                            ) : (
                                 <Button
-                                    type='button'
+                                    isLoading={isLoading}
+                                    onClick={create}
+                                    color='primary'
+                                    isDisabled={disabled}
                                     endContent={
                                         <ArrowRightIcon className='w-4' />
                                     }
-                                    key={disabled}
-                                    isDisabled={disabled}
-                                    color='primary'
-                                    // color='success'
-                                    // variant='flat'
                                 >
-                                    Next Step
+                                    Finish Setup
                                 </Button>
-                            </Link>
-                        ) : (
-                            <Button
-                                isLoading={isLoading}
-                                onClick={create}
-                                color='primary'
-                                key={disabled}
-                                isDisabled={disabled}
-                                endContent={<ArrowRightIcon className='w-4' />}
-                            >
-                                Finish Setup
-                            </Button>
-                        )}
+                            )}
+                        </div>
                     </div>
-                </div>
-                <div className='flex sticky self-start top-4 flex-col mt-1 gap-6 shrink-0 min-w-[260px]'>
-                    <div className='flex gap-3 items-center'>
-                        <div className='[&>*]:w-[20px]'>{p.icon}</div>
-                        <div className='text-xl font-semibold'>{p.name}</div>
-                    </div>
-                    {p.steps.map((stepObj, index) => {
-                        const done = index < step - 1
-                        const active = index === step - 1
-                        return (
-                            <Link
-                                href={createStepPath({
-                                    host,
-                                    provider,
-                                    step: index + 1,
-                                    hash,
-                                })}
-                                className='flex text-sm items-center  gap-2'
-                                key={stepObj.title}
-                            >
-                                <div
-                                    className={cn(
-                                        'w-[20px] h-[20px] text-sm flex items-center shadow-sm justify-center font-mono rounded-full border-gray-800 border-full',
-                                        active
-                                            ? 'bg-gray-900 text-white'
-                                            : done
-                                            ? 'bg-primary/20 '
-                                            : 'bg-gray-100',
-                                    )}
+                    <div className='flex sticky self-start top-4 flex-col mt-1 gap-6 shrink-0 min-w-[260px]'>
+                        <div className='flex gap-3 items-center'>
+                            <div className='[&>*]:w-[20px]'>{p.icon}</div>
+                            <div className='text-xl font-semibold'>
+                                {p.name}
+                            </div>
+                        </div>
+                        {p.steps.map((stepObj, index) => {
+                            const done = index < step - 1
+                            const active = index === step - 1
+                            return (
+                                <Link
+                                    href={createStepPath({
+                                        host,
+                                        provider,
+                                        step: index + 1,
+                                        hash,
+                                    })}
+                                    className='flex text-sm items-center  gap-2'
+                                    key={stepObj.title}
                                 >
-                                    {done ? (
-                                        <CheckIcon className='stroke-[3px] h-[13px]' />
-                                    ) : (
-                                        index + 1
-                                    )}
-                                </div>
+                                    <div
+                                        className={cn(
+                                            'w-[20px] h-[20px] text-sm flex items-center shadow-sm justify-center font-mono rounded-full border-gray-800 border-full',
+                                            active
+                                                ? 'bg-gray-900 text-white'
+                                                : done
+                                                ? 'bg-primary/20 '
+                                                : 'bg-gray-100',
+                                        )}
+                                    >
+                                        {done ? (
+                                            <CheckIcon className='stroke-[3px] h-[13px]' />
+                                        ) : (
+                                            index + 1
+                                        )}
+                                    </div>
 
-                                <div
-                                    className={cn(
-                                        'truncate',
-                                        active && 'font-semibold',
-                                    )}
-                                >
-                                    {stepObj.title}
-                                </div>
-                            </Link>
-                        )
-                    })}
+                                    <div
+                                        className={cn(
+                                            'truncate',
+                                            active && 'font-semibold',
+                                        )}
+                                    >
+                                        {stepObj.title}
+                                    </div>
+                                </Link>
+                            )
+                        })}
+                    </div>
                 </div>
             </div>
         </Container>
