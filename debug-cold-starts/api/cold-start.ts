b5d87274ceb58f481453dc43c7d3a22a241f50d1
remorses <beats.by.morse@gmail.com>
2023-08-17 14:09:34 +0200

    return the profile back

--- debug-cold-starts/api/cold-start.ts
+++ debug-cold-starts/api/cold-start.ts
@@ -15,43 +15,55 @@ export default async function handler(
     const session = new inspector.Session()
     session.connect()
 
-    session.post('Profiler.enable', () => {
-        // Start profiling
-        session.post('Profiler.start', async () => {
-            // Run your Node.js program or perform the operations you want to profile
-            console.time('cold start')
-            const requireDebug = false
-            const page =
-                await require('../.next/standalone/website/.next/server/pages/org/[orgId]/site/[slug]/customize.js')
-            console.log(page)
-            const appRender = page.routeModule.components.App({})
-            const pageRender = page.routeModule.userland.default({})
-            const res = await page.getServerSideProps({
-                req: { headers: {}, cookies: {} },
-                query: {},
-                res: {
-                    getHeader() {
-                        return
+    const result = await new Promise((resolve, reject) => {
+        session.post('Profiler.enable', () => {
+            // Start profiling
+            session.post('Profiler.start', async () => {
+                // Run your Node.js program or perform the operations you want to profile
+                console.time('cold start')
+                const requireDebug = false
+                const page =
+                    await require('../.next/standalone/website/.next/server/pages/org/[orgId]/site/[slug]/customize.js')
+                console.log(page)
+                const appRender = page.routeModule.components.App({})
+                const pageRender = page.routeModule.userland.default({})
+                const res = await page.getServerSideProps({
+                    req: { headers: {}, cookies: {} },
+                    query: {},
+                    res: {
+                        getHeader() {
+                            return
+                        },
                     },
-                },
-            })
-            console.log(res)
-            // execSync(`bun ${p}`, { stdio: 'inherit' })
-            console.timeEnd('cold start')
-            // Stop profiling
-            session.post('Profiler.stop', async (err, { profile }) => {
-                // Save the profile to a file
-                const profileData = JSON.stringify(profile)
-                // fs.writeFileSync('cold-start.cpuprofile', profileData)
-                // Disconnect the inspector session
-                session.disconnect()
-                const result = await uploadDirect(Buffer.from(profileData), {
-                    publicKey: '880c755228f5ed9be9dc',
-                    fileName: 'cold-start.cpuprofile',
-                    store: 'auto',
                 })
-                console.log(result)
+                console.log(res)
+                // execSync(`bun ${p}`, { stdio: 'inherit' })
+                console.timeEnd('cold start')
+                // Stop profiling
+                session.post('Profiler.stop', async (err, { profile }) => {
+                    if (err) {
+                        console.error(err)
+                        reject(err)
+                        return
+                    }
+                    // Save the profile to a file
+                    const profileData = JSON.stringify(profile)
+                    // fs.writeFileSync('cold-start.cpuprofile', profileData)
+                    // Disconnect the inspector session
+                    session.disconnect()
+                    const result = await uploadDirect(
+                        Buffer.from(profileData),
+                        {
+                            publicKey: '880c755228f5ed9be9dc',
+                            fileName: 'cold-start.cpuprofile',
+                            store: 'auto',
+                        },
+                    )
+                    resolve(result)
+                    console.log(profileData)
+                })
             })
         })
+        response.end(result)
     })
 }
