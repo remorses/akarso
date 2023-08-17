dc5dd473563b6a81c557ebb58472d17b3b3e2a93
remorses <beats.by.morse@gmail.com>
2023-08-17 14:35:49 +0200

    try bun again

--- debug-cold-starts/scripts/build.ts
+++ debug-cold-starts/scripts/build.ts
@@ -1,11 +1,45 @@
+import fs from 'fs'
+import extra from 'fs-extra'
+
 import { shell } from '@xmorse/deployment-utils/src'
+import path from 'path'
 
 async function main() {
-    await shell(`pnpm --filter website vercel-build `)
-    await shell(`cp -r ../website/.next ./.next`).catch(() => {
-        console.log('cp failed')
-        return null
+    // await shell(`pnpm --filter website vercel-build `)
+    await fs.promises
+        .rmdir(
+            path.resolve(
+                '../website/.next/standalone/node_modules/caniuse-lite',
+            ),
+            {
+                recursive: true,
+            },
+        )
+        .catch((e) => null)
+    // await fs.mkdirSync(
+    //     path.resolve('../website/.next/standalone/node_modules/'),
+    //     {
+    //         recursive: true,
+    //     },
+    // )
+    await extra.copy(
+        path.resolve('../node_modules/caniuse-lite'),
+        path.resolve('../website/.next/standalone/node_modules/caniuse-lite'),
+        {
+            // dereference: true, //
+            overwrite: true,
+        },
+    )
+    await extra.copy('../website/.next', './.next', {
+        overwrite: true,
+        dereference: true,
     })
+    // await shell(`cp -fr ../node_modules/caniuse-lite `)
+    // await shell(`cp -r ../website/.next ./.next`).catch(() => {
+    //     console.log('cp failed')
+    //     return null
+    // })
+    // https://github.com/vercel/next.js/issues/50072
     console.log('build done')
 }
 
